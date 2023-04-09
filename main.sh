02c592ed989224bb774149d00c94f92f04cc934a
Josh Murray <jarkko@example.com>
2023-04-09 20:08:41 +0300

    hey

---
+++ main.sh
@@ -0,0 +1,42 @@
+# partitioning, pacstrap and fstab
+clear
+lsblk
+read -p "enter hard drive to be installed on: " device_name
+clear
+
+(
+echo g      # new gpt partition table
+echo n      # new partition
+echo p      # primary
+echo 1      # make it first
+echo        # default starting at beginning
+echo +100m  # 100 MB more for boot partition
+echo n      # new partition
+echo p      # primary
+echo 2      # make it secound
+echo        # default starts after the previous partition
+echo        # exted the partition to the ned of the disk
+echo w      # writes changes and quits
+) | fdisk "/dev/${device_name}"
+
+mkfs.fat -F32 "/dev/${device_name}1"
+mkfs.ext4 "/dev/${device_name}2"
+
+mount "/dev/${device_name}2" /mnt
+mount --mkdir "/dev/${device_name}1" /mnt/boot/efi
+
+pacstrap /mnt base base-devel linux linux-firmware
+
+genfstab -U /mnt >> /mnt/etc/fstab
+
+# chroot
+arch-chroot /mnt -C
+"
+ln -sf /usr/share/zoneinfo/Europe/Helsinki /etc/localtime;
+whclock --systohc;
+sed -i "s/#en_US.UTF-8/en_US.UTF-8/g" /etc/locale.gen;
+locale-gen;
+pacman -S grub efibootmgr sudo networkmanager xorg gdm gnome;
+grub-install;
+grub-mkconfig -o /boot/grub/grub.cfg;
+"
